0325d788f4e010ebc03d24bf2c1357c5e59951de
Mennatallah Ashraf <72235546+Mennatallah9@users.noreply.github.com>
2023-03-09 01:31:57 +0200

    Update p2.m

--- Lab1 assignments/p2.m
+++ Lab1 assignments/p2.m
@@ -1,9 +1,3 @@
-﻿V=[2 8 7 3 1 0 8 9];
-for i=1:length(V)
-    if mod(V(i), 2)==0
-        V(i) = -1;
-    else
-        V(i) = 1;
-    end
-end
-
+V = [2 8 7 3 1 0 8 9];
+result = ones(size(V)); 
+result(mod(V, 2) == 0) = -1;
